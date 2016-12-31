4e3c1090ad7371b2fa002c2a425e13fcee368a95
David Kessler <dkessler@pillartechnology.com>
2016-12-31 15:58:41 -0600

    covered significant cases between 1 and 4999 (5000 uses a V with a line above it)

--- src/main/groovy/com/pillartechnology/numeral/RomanNumeralConverter.groovy
+++ src/main/groovy/com/pillartechnology/numeral/RomanNumeralConverter.groovy
@@ -6,7 +6,10 @@ import static com.pillartechnology.numeral.transformation.TransformerFactory.*
 
 class RomanNumeralConverter {
 
-    static final def TRANSFORMERS = [createFour(), createNine(), createTen(), createFive(), createOne()]
+    static final def TRANSFORMERS = [createFour(), createNine(), createForty(), createNinety(),
+                                     createFourHundred(), createNineHundred(),
+                                     createOne(), createFive(), createTen(), createFifty(),
+                                     createOneHundred(), createFiveHundred(), createOneThousand()]
 
     static int toNumber(String numeral) {
         Transformation transformation = new Transformation(numeral, 0)
--- src/test/groovy/com/pillartechnology/numeral/RomanNumeralConverterTest.groovy
+++ src/test/groovy/com/pillartechnology/numeral/RomanNumeralConverterTest.groovy
@@ -6,21 +6,31 @@ class RomanNumeralConverterTest {
 
     @Test
     void romanNumeralConversion() {
-        [1 : 'I',
-         2 : 'II',
-         3 : 'III',
-         4 : 'IV',
-         5 : 'V',
-         6 : 'VI',
-         7 : 'VII',
-         8 : 'VIII',
-         9 : 'IX',
-         10: 'X',
-         12: 'XII',
+        [1   : 'I', 2: 'II',
+         3   : 'III', 4: 'IV',
+         5   : 'V', 6: 'VI',
+         7   : 'VII', 8: 'VIII',
+         9   : 'IX', 10: 'X',
+         12  : 'XII', 14: 'XIV',
+         19  : 'XIX', 24: 'XXIV',
+         39  : 'XXXIX', 44: 'XLIV',
+         50  : 'L', 54: 'LIV',
+         69  : 'LXIX', 74: 'LXXIV',
+         78  : 'LXXVIII', 90: 'XC',
+         99  : 'XCIX', 100: 'C',
+         444 : 'CDXLIV', 500: 'D',
+         667 : 'DCLXVII', 900: 'CM',
+         944 : 'CMXLIV', 1000: 'M',
+         1444: 'MCDXLIV', 4999: 'MMMMCMXCIX',
         ].each { int expectedNumber, String numeral ->
             assertConversion(expectedNumber, numeral)
         }
 
+    }
+
+    @Test
+    void mixedCaseConverstion() {
+        assertConversion(8, 'ViIi')
     }
 
     static private void assertConversion(int expectedNumber, String numeral) {
